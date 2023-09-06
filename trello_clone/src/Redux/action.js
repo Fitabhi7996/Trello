48ab7ece54998e7ec8477889199d13c452e369e0
unknown <ghodmareabhishek@gmail.com>
2023-09-06 12:21:37 +0530

    done

---
+++ trello_clone/src/Redux/action.js
@@ -0,0 +1,13 @@
+// Action types for board
+export const ADD_BOARD = "ADD_BOARD";
+export const DELETE_BOARD = "DELETE_BOARD";
+
+// Action types for lists
+export const ADD_LIST = "ADD_LIST";
+export const CHANGE_LIST_TITLE = "CHANGE_LIST_TITLE";
+export const DELETE_LIST = "DELETE_LIST";
+
+// Action types for cards
+export const ADD_CARD = "ADD_CARD";
+export const CHANGE_CARD_TEXT = "CHANGE_CARD_TEXT";
+export const DELETE_CARD = "DELETE_CARD";
---
+++ trello_clone/src/seed.js
@@ -0,0 +1,79 @@
+import shortid from "shortid";
+
+export default function seed(store) {
+  // console.log("Insert first list");
+  const firstListId = shortid.generate();
+
+  store.dispatch({
+    type: "ADD_LIST",
+    payload: { listId: firstListId, listTitle: "Todo" }
+  });
+
+  store.dispatch({
+    type: "ADD_CARD",
+    payload: {
+      listId: firstListId,
+      cardId: shortid.generate(),
+      cardText: "First card"
+    }
+  });
+
+  store.dispatch({
+    type: "ADD_CARD",
+    payload: {
+      listId: firstListId,
+      cardId: shortid.generate(),
+      cardText: "Second card"
+    }
+  });
+
+  // console.log("Insert second list");
+  const secondListId = shortid.generate();
+
+  store.dispatch({
+    type: "ADD_LIST",
+    payload: { listId: secondListId, listTitle: "Doing" }
+  });
+
+  store.dispatch({
+    type: "ADD_CARD",
+    payload: {
+      listId: secondListId,
+      cardId: shortid.generate(),
+      cardText: "Card 1"
+    }
+  });
+
+  store.dispatch({
+    type: "ADD_CARD",
+    payload: {
+      listId: secondListId,
+      cardId: shortid.generate(),
+      cardText: "Card 2"
+    }
+  });
+  const thirdListId = shortid.generate();
+
+  store.dispatch({
+    type: "ADD_LIST",
+    payload: { listId: thirdListId, listTitle: "Done" }
+  });
+
+  store.dispatch({
+    type: "ADD_CARD",
+    payload: {
+      listId: thirdListId,
+      cardId: shortid.generate(),
+      cardText: "Card 1"
+    }
+  });
+
+  store.dispatch({
+    type: "ADD_CARD",
+    payload: {
+      listId: thirdListId,
+      cardId: shortid.generate(),
+      cardText: "Card 2"
+    }
+  });
+};
